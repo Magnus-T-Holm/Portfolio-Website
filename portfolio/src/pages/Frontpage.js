929f5dea8a46d9d4b586c1a3814a5a75471982cc
Magnus Holm <magnusthestrup@hotmail.com>
2023-02-17 11:37:33 +0100

    Added "hacker" effect to H1 on frontpage

--- portfolio/src/pages/Frontpage.js
+++ portfolio/src/pages/Frontpage.js
@@ -6,10 +6,49 @@ const Frontpage = () => {
   } else {
     isDanish = false
   }
+
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  let interval = null;
+
+  function glitchText(e) {
+    let iteration = 0;
+
+    clearInterval(interval);
+
+    interval = setInterval(() => {
+      e.target.innerText = e.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if (index < iteration) {
+            if (isDanish) {
+              return e.target.dataset.danish[index];
+            } else {
+              return e.target.dataset.english[index];
+            }
+          }
+
+          return letters[Math.floor(Math.random() * 26)]
+        })
+        .join("");
+      if (isDanish) {
+        if (iteration >= e.target.dataset.danish.length) {
+          clearInterval(interval);
+        }
+      } else {
+        if (iteration >= e.target.dataset.english.length) {
+          clearInterval(interval);
+        }
+      }
+
+
+      iteration += 1 / 3;
+    }, 30);
+  }
   return (
     <main id="frontpage">
       <section id="home">
-        <h1>{isDanish ? "Hej, mit navn er " : "Hello there, I'm "} Magnus Holm</h1>
+        <h1 data-danish="Hej, mit navn er Magnus Holm" data-english="Hello there, I'm Magnus Holm" onPointerEnter={glitchText}>{isDanish ? "Hej, mit navn er " : "Hello there, I'm "} Magnus Holm</h1>
         <p>{isDanish ? "En frontend web udvikler" : "A frontend web developer"}</p>
         <a href="./#projects">{isDanish ? "Projekter" : "Projects"}</a>
       </section>
